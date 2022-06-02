8526bf4a225fcfd06a068d5fafa80af11de2590d
TanjilulAnwar <tanjilulanwar@gmail.com>
2022-06-02 13:16:45 +0600

    Material nav bar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutedComponents, AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './component/shared/top-bar/top-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { NavBarComponent } from './component/shared/nav-bar/nav-bar.component';
+import { FooterComponent } from './component/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     ...AppRoutedComponents,
     TopBarComponent,
+    NavBarComponent,
+    FooterComponent
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
